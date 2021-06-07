a18da6544657b4bd35f2d1cfe075615462c07b67
actantion <672817221@qq.com>
2021-06-07 11:12:43 +0800

    add genesis block init and accounts

--- src/init/genesis_block_init.h
+++ src/init/genesis_block_init.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <map>
 #include <unordered_map>
 
 #include "common/utils.h"
@@ -21,7 +22,7 @@ private:
     void InitGenesisAccount();
     void GenerateRootAccounts();
 
-    std::unordered_map<uint32_t, std::string> pool_index_map_;
+    std::map<uint32_t, std::string> pool_index_map_;
     std::unordered_map<uint32_t, std::string> root_account_with_pool_index_map_;
 
     DISALLOW_COPY_AND_ASSIGN(GenesisBlockInit);
