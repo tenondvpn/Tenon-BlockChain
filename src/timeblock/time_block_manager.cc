61fd7a147ee931224579e344d1d3f53f9d198fba
actantion <672817221@qq.com>
2021-07-07 23:39:53 +0800

    fix clear elect node pool mod num

--- src/timeblock/time_block_manager.cc
+++ src/timeblock/time_block_manager.cc
@@ -182,32 +182,43 @@ void TimeBlockManager::UpdateTimeBlock(
 }
 
 bool TimeBlockManager::LeaderNewTimeBlockValid(uint64_t* new_time_block_tm) {
+//     auto now_tm = common::TimeUtils::TimestampSeconds();
+//     if (now_tm >= latest_time_block_tm_ + common::kTimeBlockCreatePeriodSeconds) {
+//         std::lock_guard<std::mutex> guard(latest_time_blocks_mutex_);
+//         *new_time_block_tm = latest_time_block_tm_ + common::kTimeBlockCreatePeriodSeconds;
+//         return true;
+//     }
+
     auto now_tm = common::TimeUtils::TimestampSeconds();
+    std::lock_guard<std::mutex> guard(latest_time_blocks_mutex_);
     if (now_tm >= latest_time_block_tm_ + common::kTimeBlockCreatePeriodSeconds) {
-        std::lock_guard<std::mutex> guard(latest_time_blocks_mutex_);
         *new_time_block_tm = latest_time_block_tm_ + common::kTimeBlockCreatePeriodSeconds;
+        if (now_tm - *new_time_block_tm > kTimeBlockMaxOffsetSeconds) {
+            *new_time_block_tm += kTimeBlockMaxOffsetSeconds;
+        } else {
+            *new_time_block_tm += now_tm - *new_time_block_tm;
+        }
+
         return true;
     }
-
-//     auto now_tm = common::TimeUtils::TimestampSeconds();
-//     std::lock_guard<std::mutex> guard(latest_time_blocks_mutex_);
-//     if (now_tm >= latest_time_block_tm_ + common::kTimeBlockCreatePeriodSeconds) {
-//         *new_time_block_tm = latest_time_block_tm_ + common::kTimeBlockCreatePeriodSeconds;
-//         if (now_tm - *new_time_block_tm > kTimeBlockMaxOffsetSeconds) {
-//             *new_time_block_tm += kTimeBlockMaxOffsetSeconds;
-//         } else {
-//             *new_time_block_tm += now_tm - *new_time_block_tm;
-//         }
-// 
-//         return true;
-//    
     return false;
 }
 
 bool TimeBlockManager::BackupheckNewTimeBlockValid(uint64_t new_time_block_tm) {
+//     uint64_t backup_latest_time_block_tm = latest_time_block_tm_;
+//     backup_latest_time_block_tm += common::kTimeBlockCreatePeriodSeconds;
+//     if (new_time_block_tm == backup_latest_time_block_tm) {
+//         return true;
+//     }
+// 
+//     BFT_ERROR("BackupheckNewTimeBlockValid error[%llu][%llu] latest_time_block_tm_[%lu]",
+//         new_time_block_tm, (uint64_t)backup_latest_time_block_tm, (uint64_t)latest_time_block_tm_);
+//     return false;
+
     uint64_t backup_latest_time_block_tm = latest_time_block_tm_;
     backup_latest_time_block_tm += common::kTimeBlockCreatePeriodSeconds;
-    if (new_time_block_tm == backup_latest_time_block_tm) {
+    if (new_time_block_tm < (backup_latest_time_block_tm + kTimeBlockTolerateSeconds) &&
+            new_time_block_tm >(backup_latest_time_block_tm - kTimeBlockTolerateSeconds)) {
         return true;
     }
 
