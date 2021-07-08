c99647694c394f98e4fea450e54a7062b9faa924
actantion <672817221@qq.com>
2021-07-08 15:09:53 +0800

    fix clear elect node pool mod num

--- src/block/block_manager.cc
+++ src/block/block_manager.cc
@@ -330,9 +330,9 @@ int64_t BlockManager::FixRewardWithHistory(const std::string& id, int64_t new_am
         std::string val;
         auto res = db::Db::Instance()->Get(key, &val);
         if (res.ok()) {
-            uint64_t val = 0;
-            common::StringUtil::ToUint64(val, &val);
-            account_reward_map_[key] = val;
+            uint64_t tmp_val = 0;
+            common::StringUtil::ToUint64(val, &tmp_val);
+            account_reward_map_[key] = tmp_val;
             iter = account_reward_map_.find(key);
         } else {
             account_reward_map_[key] = new_amount;
