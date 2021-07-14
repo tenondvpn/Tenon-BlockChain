e23945b024a836068132f1140006a39f62c751b1
actantion <672817221@qq.com>
2021-07-14 21:58:24 +0800

    bitmap add to block hash and agg verify it

--- src/block/db_pool_info.h
+++ src/block/db_pool_info.h
@@ -39,7 +39,7 @@ private:
     int LoadBlocksUtilLatestStatisticBlock();
     void TickSatisticBlock();
 
-    static const uint32_t kUpdateStatisticPeriod = 3000000lu;
+    static const uint32_t kUpdateStatisticPeriod = 1000000lu;
 
     std::string dict_key_;
     std::string hash_;
