68ae0df53724483191cd045a672421b08c17bd88
actantion <672817221@qq.com>
2021-07-25 20:35:04 +0800

    add gid

--- src/timeblock/time_block_manager.cc
+++ src/timeblock/time_block_manager.cc
@@ -52,7 +52,7 @@ int TimeBlockManager::LeaderCreateTimeBlockTx(transport::protobuf::Header* msg) 
 
     auto gid = common::Hash::Hash256(kTimeBlockGidPrefix +
         std::to_string(elect::ElectManager::Instance()->latest_height(
-            common::GlobalInfo::Instance()->network_id())) + std::to_string(id_test) +
+            common::GlobalInfo::Instance()->network_id())) + std::to_string(id_test++) +
         std::to_string(latest_time_block_tm_));
     uint32_t des_net_id = common::GlobalInfo::Instance()->network_id();
     dht::DhtKeyManager dht_key(des_net_id, 0);
