33aca0bced783e8943da9e991e7359a081ecfa68
actantion <672817221@qq.com>
2021-08-16 15:57:45 +0800

    support broadcast bls public key

--- src/bft/bft_manager.cc
+++ src/bft/bft_manager.cc
@@ -596,11 +596,6 @@ int BftManager::StartBft(const std::string& gid, int32_t pool_mod_index) {
 }
 
 int BftManager::AddBft(BftInterfacePtr& bft_ptr) {
-    if (common::GlobalInfo::Instance()->id() == common::Encode::HexDecode("01cab67e8eca011d1ea49177807690fa5b9958c2")) {
-        assert(bft_ptr->this_node_is_leader());
-        assert(bft_ptr->leader_mem_ptr() != nullptr);
-    }
-
     std::lock_guard<std::mutex> guard(bft_hash_map_mutex_);
     auto iter = bft_hash_map_.find(bft_ptr->gid());
     if (iter != bft_hash_map_.end()) {
