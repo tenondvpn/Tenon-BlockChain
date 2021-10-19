9218173cb79b83aceac3463105f9ea249bce8708
tenondvpn <jhsx5679@gmail.com>
2021-10-19 22:17:01 +0800

    remove libbls

--- third_party/libBLS/deps/libff/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/work/Tenon-BlockChain/third_party/libBLS/deps/libff
-# Build directory: /root/work/Tenon-BlockChain/third_party/libBLS/deps/libff/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("depends")
-subdirs("libff")
--- third_party/libBLS/deps/libff/build/depends/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/work/Tenon-BlockChain/third_party/libBLS/deps/libff/depends
-# Build directory: /root/work/Tenon-BlockChain/third_party/libBLS/deps/libff/build/depends
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
